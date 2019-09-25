63eedf03b3fff5a37abfa14af77024a56195848e
thecodinghyrax <thecodinghyrax@gmail.com>
2019-09-24 19:31:07 -0500

    Event repeating added. Some backend ui changes\

--- my_functions.py
+++ my_functions.py
@@ -1,53 +1,74 @@
-import calendar
+from datetime import date, timedelta
 
-def calendar_dict_creator(year, month):
-    '''
-        Returns a dict with a (year, month, day, day-of-week) tuple as key and name of day as value
-    '''
-    cal = calendar.Calendar(firstweekday=6)
+# today as a date object
+today = date.today()
+print('today as a date object')
+print(today)
+print(today.strftime('%Y-year, %m-month, %d-day'))
+print('-'*80)
 
-    def day_check(base, day):
-        if day == base or day == base % 7:
-            return True
-        else:
-            return False
-    cal_dict ={}
-    for day in cal.itermonthdays4(year, month):
-        day_of_week_num = day[3]
-        day_of_month = day[2]
-        if day_of_month != 0:
-            if day_check(0, day_of_week_num):
-                cal_dict[day] = 'Monday'
-            elif day_check(1, day_of_week_num):
-                cal_dict[day] = 'Tuesday'
-            elif day_check(2, day_of_week_num):
-                cal_dict[day] = 'Wednesday'
-            elif day_check(3, day_of_week_num):
-                cal_dict[day] = 'Thursday'
-            elif day_check(4, day_of_week_num):
-                cal_dict[day] = 'Friday'
-            elif day_check(5, day_of_week_num):
-                cal_dict[day] = 'Saturday'
-            elif day_check(6, day_of_week_num):
-                cal_dict[day] = 'Sunday'
-            else:
-                print('The day passed doesn\'t match a day of the week!')
+# adding days to todays date object
+new_day = today + timedelta(days=7)
+print('adding days to todays date object')
+print(new_day)
+print(type(new_day))
+print('-'*80)
 
-    return cal_dict
+# creating a timedelta object and getting the total # of seconds
+month = timedelta(days=31)
+print('creating a timedelta object and getting the total # of seconds')
+print(month.total_seconds())
+print('-'*80)
 
-# to test
-sept = calendar_dict_creator(2019, 8)
+# create a date object from a string
+date_str = '2019-12-25'
+date_obj = date.fromisoformat(date_str)
+print('create a date object from a string')
+print(date_obj)
+print(type(date_obj))
+print('-'*80)
 
-for key, value in sept.items():
-    print('{} is {}'.format(key, value))
+# mock post with repet
+print('Create a repeting post')
+# title = input('Enter the title:\n>> ')
+# post_date = input('Enter the post date as \'YYYY-MM-DD\':\n>> ')
+# post_time = input('Enter the event time as a string: \n>> ')
+# event = input('Enter the event details:\n>> ')
+# repeat = input('Enter the number of times to repeat. Leave blank if no repeat:\n>> ')
 
-# Three ways to loop through a dict
-# my_dict = {"one": 1,"two":2,"three":3,"four":4}
-# for item in my_dict:
-#     print("Key : {} , Value : {}".format(item,my_dict[item]))
+# defaults
+title = 'Devotionals'
+post_date = '2019-09-23'
+post_time = '8:00 PM to 9:00 PM'
+event = 'We will be holding our Monday night devotionals.'
+repeat = '25'
 
-# for key,value in my_dict.items():
-#     print("Key : {} , Value : {}".format(key,value))
+# repeat needs to be cast to an int so it can be used in the range function
+repeat = int(repeat)
 
-# for key in my_dict.keys():
-#     print("Key : {} , Value : {}".format(key,my_dict[key]))
+# This check will probably not be needed if the date is passed correctly from the HTML form
+while type(post_date) == str:
+    try:
+        post_date = date.fromisoformat(post_date)
+    except:
+        print('That format is not right. Use YYYY-MM-DD')
+        post_date = input('Enter the post date as \'YYYY-MM-DD\':\n>> ')
+
+# Splitting the variables allowed me use focus on the date 
+event_post_start = 'Join us for {} on '.format(title)
+event_post_end = ' at {}. {}'.format(post_time, event)
+
+# This is used to determin the type of the variables
+# print('Here is your event:\n')
+# print('title is typeof ', type(title))
+# print('post_date is typeof ', type(post_date))
+# print('post_time is typeof ', type(post_time))
+# print('event is typeof ', type(event))
+# print('repeat is typeof ', type(repeat))
+
+# a Similar loop can be used to add the entries to the db
+for post in range(repeat):
+    print(event_post_start, post_date, event_post_end)
+    week = timedelta(days=7)
+    post_date = post_date + week
+
