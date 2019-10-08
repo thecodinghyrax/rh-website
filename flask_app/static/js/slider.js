0a0b2483b893b9a495acbf74886ac0fb8f4cf760
thecodinghyrax <thecodinghyrax@gmail.com>
2019-10-07 22:50:53 -0500

    Slider is working

--- flask_app/static/js/slider.js
+++ flask_app/static/js/slider.js
@@ -3,6 +3,7 @@ const items = Array.from(newsList.children);
 const dotsNav = document.querySelector('.news__nav');
 const dots = Array.from(dotsNav.children);
 const itemWidth = items[0].getBoundingClientRect().width;
+var slideOn = true;
 
 const setItmePosition = (item, index) => {
     item.style.left = itemWidth * index + 'px';
@@ -17,6 +18,9 @@ const moveToItem = (newsList, currentItem, targetItem) => {
 };
 
 dotsNav.addEventListener('click', e => {
+    slideOn = false;
+    console.log('The slideOn var is now: ' + slideOn);
+    
     const targetDot = e.target.closest('button');
 
     if (!targetDot) return;
@@ -29,4 +33,34 @@ dotsNav.addEventListener('click', e => {
     targetDot.classList.add('current-item');
 
     moveToItem(newsList, currentItem, targetItem);
-});
+}
+);
+
+function stopSlide() {
+    clearInterval(changeSlide);
+}
+
+function changeSlide() {
+    if (slideOn === true){
+        
+        const currentItem = newsList.querySelector('.current-item');
+        const currentDot = dotsNav.querySelector('.current-item');
+        var targetIndex = dots.findIndex(dot => dot === currentDot);
+        if (targetIndex === (dots.length - 1)) {
+            targetIndex = -1;
+        };
+        const targetDot = dots[targetIndex + 1];
+        const targetItem = items[targetIndex + 1];
+        currentDot.classList.remove('current-item');
+        targetDot.classList.add('current-item');
+
+        moveToItem(newsList, currentItem, targetItem);
+    } 
+        
+};
+
+var startSlide = setInterval(changeSlide, 5000)
+
+if (slideOn === false){
+    stopSlide();
+};
