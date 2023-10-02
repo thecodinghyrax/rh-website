f949b1353f29fdd45c536059a0c256eb6630098d
thecodinghyrax <thecodinghyrax@gmail.com>
2023-10-01 20:21:26 -0500

    initial load in test env with new packages

--- flask_app/models.py
+++ flask_app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import URLSafeTimedSerializer as Serializer
 from flask_app import db, login_manager, app
 from flask_login import UserMixin
 import pytz
